26b5ef0d8d6a3b8cb987983bb77b3eb5b94955ab
whiskeyrowsoftware <100384977+whiskeyrowsoftware@users.noreply.github.com>
2024-03-07 13:48:12 -0700

    remote-02 app.config

--- projects/remote-02/src/app/app.component.ts
+++ projects/remote-02/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { AppConfig, APP_CONFIG_TOKEN } from './app.config';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'remote-02';
+  constructor(@Inject(APP_CONFIG_TOKEN) appConfig: AppConfig){
+    
+  }
 }
+
+
--- projects/remote-02/src/app/app.module.ts
+++ projects/remote-02/src/app/app.module.ts
@@ -1,18 +1,51 @@
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule, InjectionToken } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { APP_CONFIG_TOKEN,AppConfig } from './app.config';
+import { Observable, tap } from 'rxjs';
 
+const MY_APP_CONFIG: AppConfig = {
+  openAIKey: '',
+  wrsApiUrl: ''
+};
+function initializeAppFactory(httpClient: HttpClient): () => Observable<AppConfig> {
+  return () =>
+    httpClient.get<AppConfig>('./assets/app-config-info.json').pipe(
+      tap((config) =>
+        Object.assign(MY_APP_CONFIG, {
+          ...config,
+          openAIKey: config.openAIKey,
+          wrsApiUrl: `https://${config.wrsApiUrl}`,
+        })
+      ) 
+    );
+}
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    { 
+      provide: APP_INITIALIZER, 
+      useFactory: initializeAppFactory, 
+      deps: [HttpClient], 
+      multi: true 
+    },
+    { provide: APP_CONFIG_TOKEN, useValue: MY_APP_CONFIG },
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(){
+  }
+}
+
+
